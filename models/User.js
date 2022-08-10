9e6a41e85490a20f48225c13b852ef917903839c
javiermorales <javiertoto-00@hotmail.com>
2022-08-09 20:28:04 -0700

    updates

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 User.init(
   {
@@ -43,7 +47,7 @@ User.init(
     modelName: 'user',
     freezeTableName: false,
   }
-  );
+);
 
 module.exports = User;
 
